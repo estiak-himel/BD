34978f71f6cbcf1cda5b496fbc2db225fa736673
unknown <estiakhimel9@gmail.com>
2024-07-31 12:04:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
